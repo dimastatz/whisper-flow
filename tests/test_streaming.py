d1cd0618491b94638317c8015d171510fb94f6bb
Dima <dima.statz@gmail.com>
2024-06-08 21:44:45 +0300

    add tests

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -1,8 +1,18 @@
 """ test scenario module """
 
+from starlette.testclient import TestClient
+
 import whisperflow.streaming as st
+import whisperflow.fast_server as fs
 
 
 def test_streaming():
     """test hugging face image generation"""
     assert st.run(1) == 2
+
+
+def test_fast_api():
+    """test health api"""
+    client = TestClient(fs.app)
+    response = client.get("/health")
+    assert response.status_code == 200 and bool(response.text)
