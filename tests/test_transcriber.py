0f1ae4b980ed24125d8115985a511f8a928ff4d0
Dima <dima.statz@gmail.com>
2024-06-11 12:07:01 +0300

    add resources

--- tests/test_transcriber.py
+++ tests/test_transcriber.py
@@ -1,5 +1,6 @@
 """ test transcriber """
 
+import os
 import whisperflow.transcriber as tr
 
 
@@ -7,3 +8,11 @@ def test_load_model():
     """test hugging face image generation"""
     model = tr.get_model()
     assert model is not None
+
+    path = os.path.join(os.path.dirname(__file__), "./resources/3081-166546-0000.wav")
+
+    with open(path, "br") as file:
+        content = file.read()
+
+    text = tr.transcribe_pcm_chunks(model, [content])
+    assert text
