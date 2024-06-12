1a0dec898be8e69b910cd472b6196347ee4d848f
Dima <dima.statz@gmail.com>
2024-06-12 13:06:17 +0300

    compare expected and actual results

--- tests/test_transcriber.py
+++ tests/test_transcriber.py
@@ -1,7 +1,24 @@
 """ test transcriber """
 
 import os
+import json
+from jiwer import wer
 import whisperflow.transcriber as tr
+
+
+def load_resource(name: str) -> dict:
+    "load resource"
+    result = {}
+    current_path = os.path.dirname(__file__)
+    resource_name = os.path.join(current_path, f"./resources/{name}")
+
+    with open(resource_name + ".wav", "br") as file:
+        result["audio"] = file.read()
+
+    with open(resource_name + ".json", "r", encoding="utf-8") as file:
+        result["expected"] = json.load(file)
+
+    return result
 
 
 def test_load_model():
@@ -9,10 +26,10 @@ def test_load_model():
     model = tr.get_model()
     assert model is not None
 
-    path = os.path.join(os.path.dirname(__file__), "./resources/3081-166546-0000.wav")
+    resource = load_resource("3081-166546-0000")
 
-    with open(path, "br") as file:
-        content = file.read()
+    result = tr.transcribe_pcm_chunks(model, [resource["audio"]])
+    expected = resource["expected"]["final_ground_truth"]
 
-    text = tr.transcribe_pcm_chunks(model, [content])
-    assert text
+    error = wer(result["text"].lower(), expected.lower())
+    assert error < 0.1
