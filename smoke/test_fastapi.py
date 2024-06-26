7f868c3e5df6b3b2f4517bfe791115de06523e3d
Dima Statz <dima.statz@gmail.com>
2024-06-26 11:12:06 +0000

    add smoke

---
+++ smoke/test_fastapi.py
@@ -0,0 +1,14 @@
+""" integration test for web server """
+import requests
+
+
+def test_health(url="http://localhost:8181/health"):
+    """basic test"""
+    result = requests.get(url=url)
+    assert result.status_code == 200
+
+
+def test_streaming_api(chunk_size=4096):
+    """test streaming api"""
+    result = 1
+    assert result == 1
