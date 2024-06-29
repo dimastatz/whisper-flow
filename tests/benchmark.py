386b67b651d85b98f2662a9681c91fec816a9938
Dima Statz <dima.statz@gmail.com>
2024-06-29 19:40:11 +0000

    add bencharml

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,4 +1,6 @@
 """" benchmark """
+import os
+import json
 from pathlib import Path
 
 import requests
@@ -19,7 +21,36 @@ def send_chunks(url="ws://localhost:8181/ws", chunk_size=4096):
 
     websocket = ws.create_connection(url)
     websocket.settimeout(0.1)
+
+    res = load_resource("3081-166546-0000")
+    chunks = [
+        res["audio"][i : i + chunk_size]
+        for i in range(0, len(res["audio"]), chunk_size)
+    ]
+
+    print(len(chunk_size))
+
     websocket.close()
+
+
+def load_resource(name: str) -> dict:
+    "load resource"
+    result = {}
+
+    with open(get_resource_path(name, "wav"), "br") as file:
+        result["audio"] = file.read()
+
+    with open(get_resource_path(name, "json"), "r", encoding="utf-8") as file:
+        result["expected"] = json.load(file)
+
+    return result
+
+
+def get_resource_path(name: str, extension: str) -> str:
+    "get resources path"
+    current_path = os.path.dirname(__file__)
+    path = os.path.join(current_path, f"./resources/{name}")
+    return f"{path}.{extension}"
 
 
 if __name__ == "__main__":
