2839f237a447d1c859e4a30478975b51ff27f3f3
Dima Statz <dima.statz@gmail.com>
2024-07-06 16:33:41 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from whisperflow import __version__
+
+
+setup(
+    name='whisperflow',
+    version=__version__,
+
+    url='https://github.com/dimastatz/whisper-flow',
+    author='Dima Statz',
+    author_email='dima.statz@gmail.com',
+
+    py_modules=['whisperflow'],
+)
