651e0bb35c3e69218e0c71ee885b051441a1b03e
Dima Statz <dima.statz@gmail.com>
2024-08-02 15:57:27 +0000

    add benchmark

--- tests/benchmark/test_benchmark.py
+++ tests/benchmark/test_benchmark.py
@@ -28,8 +28,7 @@ def get_res(websocket):
 
 def print_result(result: dict):
     """print result and execution time"""
-    formatted_time = "{:.2f}".format(result["time"])
-    print(result["is_partial"], formatted_time, result["data"]["text"])
+    print(result["is_partial"], round(result["time"],2), result["data"]["text"])
 
 
 def test_send_chunks(url="ws://localhost:8181/ws", chunk_size=4096):
@@ -48,19 +47,22 @@ def test_send_chunks(url="ws://localhost:8181/ws", chunk_size=4096):
         websocket.send_bytes(chunk)
         res = get_res(websocket)
         if res:
-            df_result.loc[-1] = [res["is_partial"], res["time"], res["data"]["text"]]
+            df_result.loc[len(df_result)] = [res["is_partial"], round(res["time"],2), res["data"]["text"]]
 
     attempts = 0
     while attempts < 3:
         res = get_res(websocket)
         if res:
             attempts = 0
-            df_result.loc[-1] = [res["is_partial"], res["time"], res["data"]["text"]]
+            df_result.loc[len(df_result)] = [res["is_partial"], round(res["time"],2), res["data"]["text"]]
         else:
             attempts += 1
             time.sleep(1)
 
-    actual = df_result.loc[-1]["result"].lower().strip()
+    print(df_result)
+    print("Latency Stats:\n", df_result["latency"].describe())
+    
+    actual = df_result.loc[len(df_result) - 1]["result"].lower().strip()
     expected = resource["expected"]["final_ground_truth"].lower().strip()
 
     error = round(jw.wer(actual, expected), 2)
