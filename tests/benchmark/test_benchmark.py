cef20aa3789e1ecdba9d81f1175e8c442803281d
Dima Statz <dima.statz@gmail.com>
2024-08-03 13:11:58 +0000

    remove printing df

--- tests/benchmark/test_benchmark.py
+++ tests/benchmark/test_benchmark.py
@@ -28,7 +28,7 @@ def get_res(websocket):
 
 def print_result(result: dict):
     """print result and execution time"""
-    print(result["is_partial"], round(result["time"],2), result["data"]["text"])
+    print(result["is_partial"], round(result["time"], 2), result["data"]["text"])
 
 
 def test_send_chunks(url="ws://localhost:8181/ws", chunk_size=4096):
@@ -47,22 +47,30 @@ def test_send_chunks(url="ws://localhost:8181/ws", chunk_size=4096):
         websocket.send_bytes(chunk)
         res = get_res(websocket)
         if res:
-            df_result.loc[len(df_result)] = [res["is_partial"], round(res["time"],2), res["data"]["text"]]
+            df_result.loc[len(df_result)] = [
+                res["is_partial"],
+                round(res["time"], 2),
+                res["data"]["text"],
+            ]
 
     attempts = 0
     while attempts < 3:
         res = get_res(websocket)
         if res:
             attempts = 0
-            df_result.loc[len(df_result)] = [res["is_partial"], round(res["time"],2), res["data"]["text"]]
+            df_result.loc[len(df_result)] = [
+                res["is_partial"],
+                round(res["time"], 2),
+                res["data"]["text"],
+            ]
         else:
             attempts += 1
             time.sleep(1)
 
-    pd.set_option('max_colwidth', 800)
-    print(df_result.to_string(justify='left', index=False))
+    pd.set_option("max_colwidth", 800)
+    # print(df_result.to_string(justify='left', index=False))
     print("Latency Stats:\n", df_result["latency"].describe())
-    
+
     actual = df_result.loc[len(df_result) - 1]["result"].lower().strip()
     expected = resource["expected"]["final_ground_truth"].lower().strip()
 
