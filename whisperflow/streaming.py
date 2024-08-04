380f4cc10e364f18b5b1e6186d2831d2552faa48
Dima Statz <dima.statz@gmail.com>
2024-08-04 13:33:15 +0000

    fix streaming

--- whisperflow/streaming.py
+++ whisperflow/streaming.py
@@ -52,7 +52,9 @@ async def transcribe(
 
 def should_close_segment(result: dict, prev_result: dict, cycles, max_cycles=0):
     """return if segment should be closed"""
-    return result["data"] == prev_result.get("data", None) and cycles >= max_cycles
+    return cycles >= max_cycles and result["data"]["text"] == prev_result.get(
+        "data", {}
+    ).get("text", "")
 
 
 class TrancribeSession:  # pylint: disable=too-few-public-methods
