d48bae55f76c0303411a5c5c8c67138aabe2cde6
Dima Statz <dima.statz@gmail.com>
2024-09-19 18:42:29 +0000

    add long description

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 from whisperflow import __version__
 from pkg_resources import parse_requirements
 
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='whisperflow',
     version=__version__,
@@ -17,6 +21,6 @@ setup(
             Path(__file__).with_name("requirements.txt").open()
         )
     ],
-    long_description = "Whisper Flow Project",
+    long_description = long_description,
     long_description_content_type='text/markdown',
 )
