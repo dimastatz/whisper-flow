ded700f317d39c845a8a569c9fa44fede74bc6e2
Dima Statz <dima.statz@gmail.com>
2024-09-26 17:25:31 +0000

    update example

--- tests/examples/mic_transcribe.py
+++ tests/examples/mic_transcribe.py
@@ -13,12 +13,15 @@ import websockets
 async def start_transcription(url="ws://0.0.0.0:8181/ws"):
     """stream mic audio to server"""
     async with websockets.connect(url) as websocket:
-        await asyncio.gather(capture_audio(websocket), receive_transcription(websocket))
+        result = []
+        await asyncio.gather(capture_audio(websocket, result), receive_transcription(websocket, result))
+        print(f"* done recording, collecting data")
+        print("Colllected text is \n", " ".join(result))
+        
 
-
-async def capture_audio(websocket: websockets.WebSocketClientProtocol):
+async def capture_audio(websocket: websockets.WebSocketClientProtocol, result: list):
     """capture the mic stream"""
-    chunk, rate, record_sec = 1024, 16000, 5
+    chunk, rate, record_sec = 1024, 16000, 30
     p = pyaudio.PyAudio()
     stream = p.open(
         format=pyaudio.paInt16,
@@ -28,24 +31,27 @@ async def capture_audio(websocket: websockets.WebSocketClientProtocol):
         frames_per_buffer=chunk,
     )
     print("* recording")
+
     for _ in range(0, int(rate / chunk * record_sec)):
         data = stream.read(chunk)
         await websocket.send(data)
-
+        await asyncio.sleep(0.01)
+        
     stream.close()
     p.terminate()
-    print("* done recording")
+   
 
-
-async def receive_transcription(websocket):
+async def receive_transcription(websocket, result: list):
     """print transcription"""
     while True:
         try:
-            result = json.loads(websocket.recv())
-            print(result["is_partial"], round(result["time"], 2), result["data"]["text"]) 
-        except websockets.WebSocketTimeoutException:
+            await asyncio.sleep(0.01)
+            tmp = json.loads(await websocket.recv())
+            if not tmp["is_partial"]:
+                result.append(tmp["data"]["text"])
+            print(tmp["is_partial"], round(tmp["time"], 2), tmp["data"]["text"]) 
+        except Exception:
             print("No transcription available")
-            await asyncio.sleep(0.1)
-
+           
 
 asyncio.run(start_transcription())
