93a8f0b999aba6c93c0abaf11caa269cbcb4d1a0
Dima Statz <dima.statz@gmail.com>
2024-10-04 04:43:11 +0000

    add mic app

--- tests/examples/mic_transcribe.py
+++ tests/examples/mic_transcribe.py
@@ -14,10 +14,12 @@ async def start_transcription(url="ws://0.0.0.0:8181/ws"):
     """stream mic audio to server"""
     async with websockets.connect(url) as websocket:
         result = []
-        await asyncio.gather(capture_audio(websocket, result), receive_transcription(websocket, result))
+        await asyncio.gather(
+            capture_audio(websocket, result), receive_transcription(websocket, result)
+        )
         print(f"* done recording, collecting data")
         print("Colllected text is \n", " ".join(result))
-        
+
 
 async def capture_audio(websocket: websockets.WebSocketClientProtocol, result: list):
     """capture the mic stream"""
@@ -36,10 +38,10 @@ async def capture_audio(websocket: websockets.WebSocketClientProtocol, result: l
         data = stream.read(chunk)
         await websocket.send(data)
         await asyncio.sleep(0.01)
-        
+
     stream.close()
     p.terminate()
-   
+
 
 async def receive_transcription(websocket, result: list):
     """print transcription"""
@@ -49,9 +51,9 @@ async def receive_transcription(websocket, result: list):
             tmp = json.loads(await websocket.recv())
             if not tmp["is_partial"]:
                 result.append(tmp["data"]["text"])
-            print(tmp["is_partial"], round(tmp["time"], 2), tmp["data"]["text"]) 
+            print(tmp["is_partial"], round(tmp["time"], 2), tmp["data"]["text"])
         except Exception:
             print("No transcription available")
-           
+
 
 asyncio.run(start_transcription())
