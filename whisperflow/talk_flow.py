d02f8b67d7cc4da6d6d65617c694050a4040d521
dimastatz <dima.statz@gmail.com>
2025-01-20 21:49:21 +0200

    add talk flow

--- whisperflow/talk_flow.py
+++ whisperflow/talk_flow.py
@@ -1,6 +1,4 @@
-""" Implements conversation loop: capture audio -> speech to text -> custom action -> text to speech -> play audio """
-
-
-class TalkFlow:
-    def __init__(self):
-        pass
+""" 
+Implements conversation loop: capture 
+audio -> speech to text -> custom action -> text to speech -> play audio 
+"""
