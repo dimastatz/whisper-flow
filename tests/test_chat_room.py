11623cb7dcf8133148c67f30e5e006d11726c7ad
Dima Statz <dima.statz@gmail.com>
2025-01-22 19:36:31 +0000

    add chat room

--- tests/test_chat_room.py
+++ tests/test_chat_room.py
@@ -1,5 +1,6 @@
 """ test chat room """
 
+import asyncio
 import pytest
 from whisperflow.chat_room import ChatRoom
 
@@ -8,4 +9,16 @@ from whisperflow.chat_room import ChatRoom
 async def test_chat_room():
     """test chat room simple flow"""
     room = ChatRoom()
+    assert room.chat_started is False
+
+    await asyncio.gather(room.start_chat(), stop_chat_room(room))
+
+    assert room.chat_started is False
+
+
+async def stop_chat_room(room: ChatRoom):
+    """stop chat room loop"""
+    await asyncio.sleep(0.1)
+    assert room.chat_started
+    room.stop_chat()
     assert room.chat_started is False
