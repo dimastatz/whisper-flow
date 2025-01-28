b449e432cee4ceaf2bc4a34b4d9bc24ca9b959dc
Dima Statz <dima.statz@gmail.com>
2025-01-28 19:44:40 +0000

    add test

--- tests/test_chat_room.py
+++ tests/test_chat_room.py
@@ -8,7 +8,7 @@ from whisperflow.chat_room import ChatRoom
 @pytest.mark.asyncio
 async def test_chat_room():
     """test chat room simple flow"""
-    room = ChatRoom(listener, None, None)
+    room = ChatRoom(listener_mock, processor_mock, listener_mock)
     assert room.chat_started is False
 
     await asyncio.gather(room.start_chat(), stop_chat_room(room))
@@ -24,8 +24,16 @@ async def stop_chat_room(room: ChatRoom):
     assert room.chat_started is False
 
 
-async def listener(queue):
+async def listener_mock(queue):
     """collect items from queue"""
     while True:
         await asyncio.sleep(0.1)
         queue.put("hello")
+
+
+async def processor_mock(queue_in, queue_out):
+    """collect items from queue"""
+    while True:
+        await asyncio.sleep(0.1)
+        item = queue_in.get()
+        queue_out.put(item)
