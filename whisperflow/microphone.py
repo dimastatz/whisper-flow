eb8100202a0ddbe220c218648d5511e19765ecdc
Dima Statz <dima.statz@gmail.com>
2025-02-03 12:07:53 +0000

    refactor capture audio

--- whisperflow/microphone.py
+++ whisperflow/microphone.py
@@ -9,22 +9,21 @@ import pyaudio
 
 async def capture_audio(queue_chunks: queue.Queue, stop_event: asyncio.Event):
     """capture the mic stream"""
+    chunk, rate, record_sec = 1024, 16000, 1
+    audio = pyaudio.PyAudio()
+    stream = audio.open(
+        format=pyaudio.paInt16,
+        channels=1,
+        rate=rate,
+        input=True,
+        frames_per_buffer=chunk,
+    )
 
     while not stop_event.is_set():
-        chunk, rate, record_sec = 1024, 16000, 1
-        audio = pyaudio.PyAudio()
-        stream = audio.open(
-            format=pyaudio.paInt16,
-            channels=1,
-            rate=rate,
-            input=True,
-            frames_per_buffer=chunk,
-        )
-
         for _ in range(0, int(rate / chunk * record_sec)):
             data = stream.read(chunk)
             queue_chunks.send(data)
             await asyncio.sleep(0.01)
 
-        stream.close()
-        audio.terminate()
+    stream.close()
+    audio.terminate()
