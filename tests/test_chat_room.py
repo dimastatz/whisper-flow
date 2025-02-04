2230ba15e79adc2dfced8867fb009004bac49b6e
dimastatz <dima.statz@gmail.com>
2025-02-04 20:30:21 +0200

    test microphone

--- tests/test_chat_room.py
+++ tests/test_chat_room.py
@@ -3,7 +3,9 @@
 import queue
 import asyncio
 import pytest
+
 from whisperflow.chat_room import ChatRoom
+from whisperflow.microphone import capture_audio
 
 
 async def listener_mock(queue_in: queue.Queue, stop_event: asyncio.Event):
@@ -42,3 +44,18 @@ async def test_chat_room():
 
     await asyncio.gather(room.start_chat(), stop_chat())
     assert room.stop_chat_event.is_set()
+
+
+@pytest.mark.asyncio
+async def test_capture_mic():
+    """test capturing microphone"""
+    stop_event = asyncio.Event()
+    audio_chunks = queue.Queue()
+
+    async def stop_capturing():
+        await asyncio.sleep(0.1)
+        stop_event.set()
+
+    await asyncio.gather(capture_audio(audio_chunks, stop_event), stop_capturing())
+    assert stop_event.is_set()
+    assert not audio_chunks.empty()
